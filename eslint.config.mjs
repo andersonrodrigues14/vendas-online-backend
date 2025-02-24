43e118af4aafd621007edf920671d60554f6e9f4
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-02-24 10:30:58 -0300

    feat: create user (POST)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,33 +5,33 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    {
-        ignores: ['eslint.config.mjs'],
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    eslintPluginPrettierRecommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-            ecmaVersion: 5,
-            sourceType: 'module',
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/indent': ['error', 2], // Define a indentação como 4 espaços
+      'prettier/prettier': ['error', { tabWidth: 4 }], // Prettier com 4 espaços
     },
-    {
-        rules: {
-            '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-floating-promises': 'warn',
-            '@typescript-eslint/no-unsafe-argument': 'warn',
-            '@typescript-eslint/indent': ['error', 4], // Define a indentação como 4 espaços
-            'prettier/prettier': ['error', { tabWidth: 4 }], // Prettier com 4 espaços
-        },
-    },
+  },
 );
