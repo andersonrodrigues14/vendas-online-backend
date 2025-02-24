122910a3c5082d234b7695106830d945af22cdc9
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-02-24 11:11:16 -0300

    feat: create service

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  private users: User[] = [];
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+
+    const hashedPassword = await hash(createUserDto.password, saltOrRounds);
+
+    const user: User = {
+      ...createUserDto,
+      id: this.users.length + 1,
+      password: hashedPassword,
+    };
+
+    this.users.push(user);
+    return user;
+  }
+
+  async getAllUser(): Promise<User[]> {
+    return this.users;
+  }
+}
